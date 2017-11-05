0e973e76fcdbd59b03a6bc318acf65d838faf88f
JiahuiRuan <jr58@rice.edu>
2017-11-04 22:37:21 -0500

    add resources

---
+++ HOC/src/actions/index.js
@@ -0,0 +1,10 @@
+import {
+  CHANGE_AUTH
+} from './index';
+
+export function authenticate(isLoggedIn) {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
+  }
+}
---
+++ HOC/src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super special recipe
+      <ul>
+        <li>Suger</li>
+        <li>Onion</li>
+        <li>Salt</li>
+      </ul>
+    </div>
+  )
+}
